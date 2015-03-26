bddbc418ff1c7840fd8b0e071f9c20a408e5c656
javinc <javinczki02@gmail.com>
2015-03-26 21:20:54 +0000

    stat added

--- App/app.js
+++ App/app.js
@@ -1,16 +1,17 @@
 angular.module('glyus', [])
 .controller('ShorterController', ['$http', '$scope', function($http, $scope) {
     $scope.create = function() {
+        getById('clicks_text').hidden = true;
         if($scope.url != undefined && $scope.url != '') {
             debug('creating...')
-
             $http.get(api + '/create?url=' + $scope.url).
 	        success(function(data) {
 	            debug('success!')
 	            debug(data)
 
 	            $scope.new = host + '/' + data.new;
-	            $scope.clicks = data.clicks + ' clicks';
+	            $scope.clicks = data.clicks;
+	            getById('clicks_text').hidden = false;
 	        }).
 	        error(function(data) {
 	            debug('error!')
@@ -20,10 +21,32 @@ angular.module('glyus', [])
 	        })
         }
     };
+
+    // update stat
+    setInterval(function() {
+    	debug('update stat')
+    	$http.get(api + '/stats').
+        success(function(data) {
+            debug('success!')
+            debug(data.total)
+
+            getById('total_text').innerHTML = data.total;
+        })
+    }, 1000)
 }]);
 
 var host = 'glyus.dev';
 var api = 'http://api.glyus.dev';
+
 var debug = function(x) {
-	// console.log(x)
+	console.log(x)
 }
+
+var getById = function(selector) {
+    return document.getElementById(selector)
+}
+
+function urlChecker(s) {    
+      var regexp = /(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/;
+      return regexp.test(s);    
+ }
