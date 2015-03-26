1cc104ad50cd57924ad2df8d373bed9d51743512
javinc <javinczki02@gmail.com>
2015-03-26 21:43:28 +0000

    config added

---
+++ Api/Page/Stats.php
@@ -0,0 +1,44 @@
+<?php //-->
+/*
+ * This file is part of the Openovate Labs Inc. framework library
+ * (c) 2013-2014 Openovate Labs
+ *
+ * Copyright and license information can be found at LICENSE
+ * distributed with this package.
+ */
+
+namespace Api\Page;
+
+/**
+ * The base class for any class that defines a view.
+ * A view controls how templates are loaded as well as 
+ * being the final point where data manipulation can occur.
+ *
+ * @vendor Openovate
+ * @package Framework
+ */
+class Stats extends \Page 
+{
+	/* Constants
+	-------------------------------*/
+	const WORDGEN_HOST = 'http://randomword.setgetgo.com/get.php';
+
+	/* Public Properties
+	-------------------------------*/
+	/* Protected Properties
+	-------------------------------*/
+	/* Public Methods
+	-------------------------------*/
+	public function getVariables()
+	{
+		// check if new exists
+		$links = control()->database()->search('link')->setColumns('count(id) as total')->getRow();
+
+		return $links;
+	}
+
+	/* Protected Methods
+	--------------------------------------------*/
+	/* Private Methods
+	--------------------------------------------*/
+}
