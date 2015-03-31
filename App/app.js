2a69be5044ecc55ef215279583564e74a6cd7bcb
smartbox <smartbox@smartbox-unit.(none)>
2015-03-31 16:15:27 +0800

    clean code

--- App/app.js
+++ App/app.js
@@ -9,15 +9,15 @@ angular.module('glyus', [])
 	            debug('success!')
 	            debug(data)
 		    
-		    if(data == null) {
-			return;
-                    }
+                if(data == null) {
+                    return;
+                }
   
 	            $scope.url = host + '/' + data.new;
-		    getById('gly_text').select()
-    
-                    $scope.clicks = data.clicks;
-	            $scope.new = data.new;
+                $scope.clicks = data.clicks;
+                $scope.new = data.new;
+		        
+                getById('gly_text').select()
 	            getById('clicks_text').hidden = false;
 	        }).
 	        error(function(data) {
@@ -51,24 +51,32 @@ var getById = function(selector) {
 }
 
 function urlChecker(s) {    
-      var regexp = /(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/;
-      return regexp.test(s);    
+    var regexp = /(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/;
+    return regexp.test(s);    
 }
 
-  window.fbAsyncInit = function() {
+window.fbAsyncInit = function() {
     FB.init({
-      appId      : '470941366421345',
-      xfbml      : true,
-      version    : 'v2.3'
+        appId      : '470941366421345',
+        xfbml      : true,
+        version    : 'v2.3'
     });
-  };
+};
 
-  (function(d, s, id){
-     var js, fjs = d.getElementsByTagName(s)[0];
-     if (d.getElementById(id)) {return;}
-     js = d.createElement(s); js.id = id;
-     js.src = "//connect.facebook.net/en_US/sdk.js";
-     fjs.parentNode.insertBefore(js, fjs);
-   }(document, 'script', 'facebook-jssdk'));
+// facebook
+(function(d, s, id){
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) {return;}
+    js = d.createElement(s); js.id = id;
+    js.src = "//connect.facebook.net/en_US/sdk.js";
+    fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
 
+// Google Analytics
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
+ga('create', 'UA-53683442-4', 'auto');
+ga('send', 'pageview');
--- App/index.php
+++ App/index.php
@@ -29,21 +29,22 @@
 <html ng-app="glyus" prefix="og: http://ogp.me/ns#">
     <head>
         <title>Gly - Easy to remember URL shortener</title>
-	<link rel="shortcut icon" type="image/png" href="/icon.png"/>
+    	<link rel="shortcut icon" type="image/png" href="/icon.png"/>
+    
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="keywords" content="chiligarlic, gly, shortlink, shortener, linker">
-        <meta name="description" content="Gly an easy to remember URL shortener, Just another link shortener with a litle bit of twist">
+        <meta name="description" content="Gly an easy to remember URL shortener">
         <meta name="author" content="chiligarlic">
 
-<meta property="og:title" content="Gly - Easy to remember URL shortener" />
-<meta property="og:site_name" content="Gly"/>
-<meta property="og:url" content="http://gly.us/" />
-<meta property="og:description" content="Gly an easy to remember URL shortener" />
-<meta property="og:image" content="http://gly.us/logo.png" />
-<meta property="fb:app_id" content="470941366421345" />
-<meta property="og:type" content="website" />
+        <meta property="og:title" content="Gly - Easy to remember URL shortener" />
+        <meta property="og:site_name" content="Gly"/>
+        <meta property="og:url" content="http://gly.us/" />
+        <meta property="og:description" content="Gly an easy to remember URL shortener" />
+        <meta property="og:image" content="http://gly.us/logo.png" />
+        <meta property="fb:app_id" content="470941366421345" />
+        <meta property="og:type" content="website" />
 
         <!-- Latest compiled and minified JQuery -->
         <script src="//code.jquery.com/jquery-1.11.2.min.js"></script>
@@ -56,39 +57,27 @@
         <!-- Latest compiled and minified Angular -->
         <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.3.15/angular.min.js"></script>
         
+        <!-- App Script -->
         <link rel="stylesheet" href="app.css">
         <script src="config.js"></script>
         <script src="app.js"></script>
     </head>
     <body>
         <div ng-controller="ShorterController" align="center" id="container">
-	    <h1>
-		<img src="/icon.png">Gly
-		<span id="total_text">0</span>
+    	    <h1>
+        		<img src="/icon.png">Gly
+        		<span id="total_text">0</span>
             </h1>
             <form ng-submit="create()">
                 <input id="gly_text" class="glyus" type="text" ng-model="url" autofocus required placeholder="Let's simlpify your URL">
             </form>
-            <!-- <p class="new">{{new}}</p> -->
             <br />
             <p id="clicks_text" hidden>clicks <span class="badge">{{clicks}}</span></p>
-<br /><br /><br /><br /><br />	
-<div
-  class="fb-like"
-data-action="like"
-  data-layout="button_count"
-  data-show-faces="true">
+            <br /><br /><br /><br /><br />	
+            <div class="fb-like" data-action="like" data-layout="button_count" data-show-faces="true"></div>
         </div>
-</div>
-	<a href="mailto:dev@chiligarlic.com"><img src="http://chiligarlic.com/img/favicon.png" alt="ChiliGarlic"></a>
-        <script>
-            (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-            })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-            ga('create', 'UA-53683442-4', 'auto');
-            ga('send', 'pageview');
-        </script>
+        <a href="mailto:dev@chiligarlic.com">
+            <img src="http://chiligarlic.com/img/favicon.png" alt="ChiliGarlic">
+        </a>
     </body>
 </html>
