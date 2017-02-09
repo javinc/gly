1770a806c2b7a821fb908deac7099756547f0700
James <javinczki02@gmail.com>
2017-02-09 19:26:49 +0800

    Update app.js
    update GA

--- App/app.js
+++ App/app.js
@@ -79,5 +79,5 @@ window.fbAsyncInit = function() {
 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-ga('create', 'UA-53683442-4', 'auto');
+ga('create', 'UA-53683442-1', 'auto');
 ga('send', 'pageview');
